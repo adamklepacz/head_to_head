48857804d7810a118863e7d54f9e85aff5d6b238
Adam Klepacz <adamklepacz92@gmail.com>
2018-01-22 11:30:39 +0100

    Adding, removing players from list

---
+++ 01-Start Copy/src/js/App.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+// import NavBar from './components/NavBar';
+// import Home from './components/Home';
+// import Login from './components/Login';
+// import All from './components/All';
+import Admin from './components/admin/Admin';
+// import Loader from './components/Loader';
+import { NavBar, Home, Login, All, Loader } from './components';
+import { Route, Switch } from 'react-router-dom';
+import ViewStore from './stores/ViewStore';
+import DevTools from 'mobx-react-devtools';
+import { observer } from 'mobx-react';
+import { withRouter } from 'react-router-dom';
+
+interface AppProps {
+  viewStore: ViewStore
+};
+
+interface AppState {
+
+};
+
+@observer
+class App extends React.Component<AppProps, AppState> {
+ 
+  constructor(props){
+    super(props);
+
+  }
+
+  componentDidMount() {
+    const { viewStore } = this.props;
+    setTimeout(() => {
+      viewStore.firebaseCheckAuth();
+    }, 800);  
+  }
+
+  render(){
+    const { viewStore } = this.props;
+    const { isLoading } = viewStore;
+
+    return (
+      <div className={`${isLoading ? 'is-loading' : ''}`}>
+        {
+          <DevTools />
+        }
+        {/* NavBar - do I need to include the ending tag? :) */}
+        {
+          isLoading ? <Loader /> : 
+          <div>
+            <NavBar viewStore={viewStore}/>
+
+            <div className="container-fluid">
+              <div className="row">
+                <div className="container main-content">
+                  <div className="row">
+
+                    {/* Main content - start */}
+                    <div className={`col-sm-12`} >
+                      <Switch>
+                        <Route exact path="/" component={ Home }/> 
+                        {/* <Route path="/login" component={ Login } /> */}
+                        <Route 
+                          path="/login" 
+                          render={routeProps => <Login {...routeProps} viewStore={viewStore}/>} 
+                        />
+                        <Route path="/all" component={ All } />
+                        <Route path="/admin" component={ Admin } />
+                      </Switch>
+                    </div>
+                    {/* Main content - end */}
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default withRouter(App);
---
+++ 01-Start Copy/src/js/utils/firebase.ts
@@ -0,0 +1,47 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBb6qwPOYcdhnh1_qIKTsLF_-I3a8anTU0",
+  authDomain: "head-to-head-8efe2.firebaseapp.com",
+  databaseURL: "https://head-to-head-8efe2.firebaseio.com",
+}
+
+firebase.initializeApp(config)
+
+export const ref = firebase.database().ref();
+export const firebaseAuth = firebase.auth;
+
+// references to out children in our database
+// export const headToHeadsRef = ref.child('headToHeads');
+export const playersRef = ref.child('players');
+// export const gamesRef = ref.child('games');
+
+export function auth (email, pw) {
+  return firebaseAuth().createUserWithEmailAndPassword(email, pw)
+    .then(saveUser)
+}
+
+export function login (email, pw) {
+  return firebaseAuth().signInWithEmailAndPassword(email, pw)
+}
+
+export function logout () {
+  return firebaseAuth().signOut()
+}
+
+export function resetPassword (email) {
+  return firebaseAuth().sendPasswordResetEmail(email)
+}
+
+export function off () {
+  return ref.off()
+}
+
+export function saveUser (user) {
+  return ref.child(`users/${user.uid}/info`)
+    .set({
+      email: user.email,
+      uid: user.uid
+    })
+    .then(() => user)
+}
